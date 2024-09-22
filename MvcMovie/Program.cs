e05d1990236a7e11498af4aa2859f85bcc0e5fce
Ангелина Балаян <bangel28@mail.ru>
2024-09-22 19:29:47 +0300

    пункт 4

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
